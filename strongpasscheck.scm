502e7f140d524b8d4361d0ebd92fcbee2ed579c4
jrciii <josephcooperiii@gmail.com>
2021-09-21 10:25:04 -0400

    refactoring strongpasscheck. BITFLAG!

--- strongpasscheck.scm
+++ strongpasscheck.scm
@@ -10,26 +10,51 @@
 ;; Insert one character to password,
 ;; Delete one character from password, or
 ;; Replace one character of password with another character.
-(define has-lower #f)
-(define has-upper #f)
-(define has-digit #f)
-
+;; (define has-lower #f)
+;; (define has-upper #f)
+;; (define has-digit #f)
+(require rnrs/arithmetic/bitwise-6)
 (define/contract (strong-password-checker password)
   (-> string? exact-integer?)
-  (set! has-lower #f)
-  (set! has-upper #f)
-  (set! has-digit #f)
-  (let ((clist (string->list password)))
-    (for-each (lambda (c)
-		(set! has-lower (or has-lower (char-lower-case? c)))
-		(set! has-upper (or has-upper (char-upper-case? c)))
-		(set! has-digit (or has-digit (char-numeric? c))))
-	      clist)
-    (strengthen 0 0 clist)))
+  ;; (set! has-lower #f)
+  ;; (set! has-upper #f)
+  ;; (set! has-digit #f)
+  ;; (let ((clist (string->list password)))
+  ;;   (for-each (lambda (c)
+  ;; 		(set! has-lower (or has-lower (char-lower-case? c)))
+  ;; 		(set! has-upper (or has-upper (char-upper-case? c)))
+  ;; 		(set! has-digit (or has-digit (char-numeric? c))))
+  ;; 	      clist)
+  ;;   (strengthen 0 0 clist))
+  (strengthen 0 0 (string->list password) 0 #f #f #f)
+  )
 
 (define (btoi b)
   (if b 1 0))
 
+(define HAS_LOWER 1)
+(define HAS_UPPER 2)
+(define HAS_DIGIT 4)
+
+(define (set-flag bitflag flag)
+  (bitwise-ior flags flag))
+
+(bitwise-bit-count 7)
+
+(define (add-flag flags)
+  (bitwise-ior flags (bitwise-arithmetic-shift 1 (bitwise-first-bit-set (bitwise-not flags)))))
+
+(define (strengthen steps pos remaining last rep flags)
+  (let ((chars-left (max 0 (- 6 pos)))
+	(curr-length (+ pos (length remaining)))
+	(spc-chr-left (bitwise-bit-count flags)))
+(cond
+ [(null? remaining)
+  (+ steps (max chars-left spc-chr-left))]
+ [(= 3 rep)
+  (cond
+   [(> spc-chr-left 0)  ])])))
+  
 (define (strengthen steps pos remaining)
   (let ((charsLeft (max 0 (- 6 pos)))
 	(curr-length (+ pos (length remaining)))
@@ -52,19 +77,41 @@
 	 (cdr remaining)))]
      [(null? remaining)
       (+ steps (max charsLeft spc-chr-left))]
-     [(and (< pos 18) (>= (length remaining) 3) (= 1 (length (remove-duplicates (take remaining 3)))))
-      (cond [(and (>= curr-length 6) (<= curr-length 20))
+     [(and (>= (length remaining) 3) (= 1 (length (remove-duplicates (take remaining 3)))))
+      (cond [(and
+	      (>= curr-length 6)
+	      (or
+	       (> spc-chr-left 0)
+	       (= (length remaining) 3)
+	       (and (<= curr-length 20) eq? (third remaining) (fourth remaining))))
 	     (set! has-digit (or has-upper has-digit))
 	     (set! has-upper (or has-lower has-upper))
 	     (set! has-lower #t)
 	     (strengthen (add1 steps) (+ 3 pos) (drop remaining 3))]
-	    [(and (< curr-length 20) (<= (+ pos charsLeft) 6))
+	    [(< curr-length 6)
 	     (strengthen (add1 steps) (+ 2 pos) (cdr remaining))]
+	    [(> curr-length 20)
+	     (strengthen (add1 steps) pos (cdr remaining))]
 	    [else (strengthen (add1 steps) pos (cdr remaining))])]
      [else (strengthen steps (add1 pos) (cdr remaining))])))
 
-;(strong-password-checker "aaa111")
-;(strong-password-checker "aaaB1")
-;(strong-password-checker "1111111111")
-; Gives 9 should give 8
-(strong-password-checker "bbaaaaaaaaaaaaaaacccccc")
+(map strong-password-checker '("a"
+			       "aaa111"
+			       "aaaB1"
+			       "1111111111"
+			       "bbaaaaaaaaaaaaaaacccccc"
+			       "aaa123"
+			       "aaaabbbbccccddeeddeeddeedd"
+			       "1337c0DE"
+			       "aaaaAAAAAA000000123456"
+			       "A1234567890aaabbbbccccc"))
+;; 5
+;; 2
+;; 1
+;; 3
+;; 8
+;; 1
+;; 8
+;; 0
+;; 5
+;; 4
